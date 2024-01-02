9014809e7ee3ec76849d91a3fde549516cea5c05
hamidreza <dolati.hamidreza78@gmail.com>
2024-01-02 11:54:08 +0330

    commenting out about api key

--- getWeather.py
+++ getWeather.py
@@ -6,6 +6,9 @@ import os
 using weather api : https://www.weatherapi.com/
 """
 
+"""
+get api_key from web_site and put it in a .env file as key/value pairs
+"""
 load_dotenv("./.env")
 api_key = os.environ.get("api_key")  # or os.getenv("api_key")
 
